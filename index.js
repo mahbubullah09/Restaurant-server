d682cabc82564f0c3e8ec75c12adbddc631e5f5a
mahbubullah09 <mahbubullahpathan@gmail.com>
2024-04-03 16:48:38 +0600

    delete menu

--- index.js
+++ index.js
@@ -170,13 +170,23 @@ async function run() {
       res.send(menu)
     })
 
-    app.post('/menu', verifyToken, async (req, res) => {
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
 
       const menu = req.body;
       const result = await menuCollection.insertOne(menu);
       res.send(result)
 
     })
+
+    app.delete('/menu/:id', verifyToken, async (req, res) => {
+
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+
+      const result = await menuCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
 
 
